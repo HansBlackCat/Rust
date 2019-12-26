1d2826de0ff5b36487f5a3739b002ac0fe2e9d98
Hans <jeremy3599@snu.ac.kr>
2019-12-26 17:06:07 +0900

    cargo init

---
+++ hello_cargo/target/debug/deps/hello_cargo-b22c5f21db270d0b.d
@@ -0,0 +1,5 @@
+C:\Users\Hans\Git\Rust\hello_cargo\target\debug\deps\hello_cargo-b22c5f21db270d0b.rmeta: src\main.rs
+
+C:\Users\Hans\Git\Rust\hello_cargo\target\debug\deps\hello_cargo-b22c5f21db270d0b.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Hans\Git\Rust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Hans\Git\Rust\hello_cargo\src\main.rs
