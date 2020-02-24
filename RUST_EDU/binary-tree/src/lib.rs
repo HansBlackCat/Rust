88bc5eb9fb1475d924d131b4b4026371d9e549ce
HansBlackCat <jeremy3599@snu.ac.kr>
2020-02-24 18:38:35 +0900

    2020-02-24 18:38:30

--- RUST_EDU/binary-tree/src/lib.rs
+++ RUST_EDU/binary-tree/src/lib.rs
@@ -5,10 +5,10 @@ mod tests {
     #[test]
     fn main() {
         use crate::binary_tree::*;
-        println!("Testttttttttttttt");
-        BinaryTree::Empty.insert(4).simple_print();
-        BinaryTree::singleton(10).insert(3).simple_print();
-        BinaryTree::from_vec(&vec![9]).simple_print();
+        //println!("Testttttttttttttt");
+        //BinaryTree::Empty.insert(4).simple_print();
+        //BinaryTree::singleton(10).insert(3).simple_print();
+        BinaryTree::from_vec(&vec![5,3,7,2,6,3,9,7,11,13,1]).simple_print();
     }
 }
 
